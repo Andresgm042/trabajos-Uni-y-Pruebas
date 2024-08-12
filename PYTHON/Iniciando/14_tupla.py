fa5287a907fba8993cd6259f3a7a7b0ea5c9cde6
Andresgm042 <andresfe0815@gmail.com>
2024-08-12 18:17:06 -0500

    updated

---
+++ PYTHON/Iniciando/14_tupla.py
@@ -0,0 +1,10 @@
+tupla1 = (2, 4, 6)
+tupla2 = ("lapiz", "borrados", "sacapunta")
+tupla3 = (1, "palabra", 3.99)
+#las tuplas son parecidas a listas pero son inmutables
+
+print(tupla1)
+print(tupla2)
+print(tupla3)
+
+print(tupla1[0])
---
+++ PYTHON/Iniciando/15_tupla2.py
@@ -0,0 +1,8 @@
+def datos():
+    return 10, 20
+
+x, y = datos()
+print(x)
+print(y)
+print(x, y)
+print(x * y)
